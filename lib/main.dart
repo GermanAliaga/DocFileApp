006753c12adb036a65710470a3e4abc6440ef247
GermanAliaga <usami.haruka01@gmail.com>
2025-05-13 01:30:27 -0400

    Splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:docfileapp/pages/myhomepage.dart';
+import 'package:docfileapp/pages/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,1 +1,56 @@
+import 'dart:async';
 
+import 'package:docfileapp/pages/myhomepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String logo = "assets/icons/216411_star_icon.svg";
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const MyHomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromARGB(248, 179, 255, 252),
+        Color.fromARGB(248, 87, 221, 255),
+        Color.fromARGB(255, 33, 103, 182)
+      ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            logo,
+            semanticsLabel: 'Logo',
+            width: 200,
+          )
+        ],
+      ),
+    ));
+  }
+}
